3b996323fc439c0a51ccd34ddc061c86f0765065
Lionel Usubelli <lionel.usubelli@ext.bpifrance.fr>
2020-03-27 14:40:11 +0100

    [TECH] tototototo

--- accounting.sh
+++ accounting.sh
@@ -1,4 +1,6 @@
 #!bin/bash
 
+docker-compose -f accounting-infrastructure.yml down
 docker-compose -f accounting-infrastructure.yml rm -f
-docker-compose -f accounting-infrastructure.yml up
+docker-compose -f accounting-infrastructure.yml up -d
+docker-compose -f accounting-infrastructure.yml logs -f user-api organization-api backend frontend webconsole mongodb 2>&1 | tee logs/logs.log &
--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -21,3 +21,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo usermod -aG docker jenkins
+
+sudo usermod -aG docker $USER
+
+newgrp docker
